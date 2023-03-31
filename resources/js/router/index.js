bf20c771d8e14e3e82df290d8e2fd402ae5bd24c
ferran <ferransaez2000@gmail.com>
2023-03-31 17:26:28 +0200

    #

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,14 +1,13 @@
+import {createWebHistory, createRouter} from "vue-router";
 import Home from '../components/Home.vue';
 import Register from '../components/Register.vue';
 import Login from '../components/Login.vue';
 import Dashboard from '../components/Dashboard.vue';
 
 
-
-
 export const routes = [{
         name: 'home',
-        path: '/home',
+        path: '/',
         component: Home
     },
     {
@@ -27,3 +26,10 @@ export const routes = [{
         component: Dashboard
     },
 ];
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: routes,
+  });
+  
+  export default router;
