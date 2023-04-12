74886db891789bdb7a78ac59ba84b75ce90927c6
ferran <ferransaez2000@gmail.com>
2023-04-12 20:14:56 +0200

    #

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,17 +2,20 @@
     <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-light bg-light ">
       <div class="container-fluid pb-3">
-        <router-link to="/" class="nav-item nav-link" style="font-family: twitch-logo;">URBAN <br>MUSIC</router-link>
+        <router-link to="/" class="nav-item nav-link text-color1 fs-5" style="font-family: twitch-logo;">URBAN <br>MUSIC</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" v-if="isLoggedin">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <router-link to="/dashboard" class="nav-item nav-link" >Dashboard</router-link>
+                <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5 fw-bold">Categorias</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="/posts" class="nav-item nav-link">xxxx</router-link>
+                <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5 fw-bold">Top Canciones</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/posts" class="nav-item nav-link mt-3 ms-5 fw-bold">Merchandising Artistas</router-link>
             </li>
           </ul>
           <form class="d-flex">
@@ -22,20 +25,20 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent"  v-else>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-            <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5" >Dashboard</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/posts" class="nav-item nav-link mt-3 ms-5">xxxx</router-link>
-        </li>
-        </ul>
-            <router-link to="/login" class="nav-item nav-link mt-3 me-5"><button>Iniciar Sesión</button></router-link>
-            <router-link to="/register" class="nav-item nav-link mt-3 me-5"><button>Registrarse</button></router-link>
+                    <router-link to="/dashboard" class="nav-item nav-link  mt-3 ms-5 fw-bold">Categorias</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/dashboard" class="nav-item nav-link  mt-3 ms-5 fw-bold">Top Canciones</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/posts" class="nav-item nav-link  mt-3 ms-5 fw-bold">Merchandising Artistas</router-link>
+                </li>
+            </ul>
+            <router-link to="/login" class="nav-item nav-link mt-3 me-5"><button class="fondo-color fw-bold">Iniciar Sesión</button></router-link>
+            <router-link to="/register" class="nav-item nav-link mt-3 me-5"><button class="fondo-color fw-bold">Registrarse</button></router-link>
         </div>
       </div>
-    </nav>
-    
-    
-           
+    </nav>    
     
             <router-view></router-view>
     </div>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,60 @@
 <template>
-    <p>Home</p>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../../img/car1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Etiqueta de la primera diapositiva</h5>
+        <p>Algún contenido placeholder representativo para la primera diapositiva.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../../img/car2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Etiqueta de la segunda diapositiva</h5>
+        <p>Algún contenido placeholder representativo para la segunda diapositiva.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../../img/car3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Etiqueta de la tercera diapositiva</h5>
+        <p>Algún contenido placeholder representativo para la tercera diapositiva.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Anterior</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Siguiente</span>
+  </button>
+</div>
+<div class="container mt-5">
+<h2>Artistas más escuchados</h2>
+
+  <div class="row align-items-start">
+    <div class="col-lg-4 mt-5 mb-5">
+        <img src="../../img/quevedo.jpg" class="d-block mx-auto " alt="...">
+        <p class="text-center mt-2">Quevedo</p>
+    </div>
+    <div class="col-lg-4 mt-5 mb-5">
+        <img src="../../img/badbunny.jpg" class="d-block  mx-auto " alt="...">
+        <p class="text-center mt-2">Bad Bunny</p>
+    </div>
+    <div class="col-lg-4 mt-5 mb-5">
+        <img src="../../img/shakira.jpg" class="d-block  mx-auto" alt="...">
+        <p class="text-center mt-2">Shakira</p>
+    </div>
+</div>
+</div>
 </template>
 
 <script>
