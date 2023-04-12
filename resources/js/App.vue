d07fcee4ffa5c43a22b9274a73286f243ace53e8
Ferran <ferransaez2000@gmail.com>
2023-04-12 11:15:06 +0200

    #

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,24 +1,45 @@
 <template>
-    <div class="container">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
-            <a class="navbar-brand" href="#">Laravel Vue 3</a>
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation"></button>
-            <div class="navbar-nav" v-if="isLoggedin">
+    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link to="/" class="nav-item nav-link">Home</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" v-if="isLoggedin">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
                 <router-link to="/dashboard" class="nav-item nav-link">Dashboard</router-link>
+            </li>
+            <li class="nav-item">
                 <router-link to="/posts" class="nav-item nav-link">xxxx</router-link>
-                <a class="nav-item nav-link" style="cursor: pointer;" @click="logout">Logout</a>
-            </div>
-            <div class="navbar-nav" v-else>
-                <router-link to="/" class="nav-item nav-link">Home</router-link>
-                <router-link to="/login" class="nav-item nav-link">Login</router-link>
-                <router-link to="/register" class="nav-item nav-link">Register</router-link>
-            </div>
-        </nav>
- 
-        <router-view></router-view>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <a class="nav-item nav-link" style="cursor: pointer;" @click="logout">Logout</a>
+          </form>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"  v-else>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+            <router-link to="/dashboard" class="nav-item nav-link">Dashboard</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/posts" class="nav-item nav-link">xxxx</router-link>
+        </li>
+        </ul>
+            <router-link to="/login" class="nav-item nav-link">Login</router-link>
+            <router-link to="/register" class="nav-item nav-link">Register</router-link>
+        </div>
+      </div>
+    </nav>
+    
+    
+           
+    
+            <router-view></router-view>
     </div>
- </template>
+    </template>
  <script>
  export default {
     name: "App",
