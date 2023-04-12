7ba0072152d6483871b44fc172fdebbe2eb9d33b
ferran <ferransaez2000@gmail.com>
2023-04-12 17:42:19 +0200

    #

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <router-link to="/" class="nav-item nav-link">Home</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <div class="container-fluid pb-3">
+        <router-link to="/" class="nav-item nav-link" style="font-family: twitch-logo;">URBAN <br>MUSIC</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" v-if="isLoggedin">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <router-link to="/dashboard" class="nav-item nav-link">Dashboard</router-link>
+                <router-link to="/dashboard" class="nav-item nav-link" >Dashboard</router-link>
             </li>
             <li class="nav-item">
                 <router-link to="/posts" class="nav-item nav-link">xxxx</router-link>
@@ -22,14 +22,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent"  v-else>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-            <router-link to="/dashboard" class="nav-item nav-link">Dashboard</router-link>
+            <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5" >Dashboard</router-link>
         </li>
         <li class="nav-item">
-            <router-link to="/posts" class="nav-item nav-link">xxxx</router-link>
+            <router-link to="/posts" class="nav-item nav-link mt-3 ms-5">xxxx</router-link>
         </li>
         </ul>
-            <router-link to="/login" class="nav-item nav-link">Login</router-link>
-            <router-link to="/register" class="nav-item nav-link">Register</router-link>
+            <router-link to="/login" class="nav-item nav-link mt-3 me-5"><button>Iniciar Sesión</button></router-link>
+            <router-link to="/register" class="nav-item nav-link mt-3 me-5">Registrarse</router-link>
         </div>
       </div>
     </nav>
