90d86737289cf515ba942f93b17b69c4ff3e2d1e
ferran <ferransaez2000@gmail.com>
2023-04-14 17:26:12 +0200

    #

--- database/migrations/2023_04_14_135949_create_categoria_canciones_table.php
+++ database/migrations/2023_04_14_135949_create_categoria_canciones_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('canciones', function (Blueprint $table) {
+        Schema::create('categoria_canciones', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('audio');
             $table->string('image');
-            $table->unsignedBigInteger('id_categoria_cancion');
             $table->timestamps();
-            $table->foreign('id_categoria_cancion')->references('id')->on('categorias_cancion')->onDelete('cascade');
-            
         });
     }
 
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('canciones');
+        Schema::dropIfExists('categoria_canciones');
     }
 };
