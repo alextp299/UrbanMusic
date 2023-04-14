bcbd494bd2ee4488e9251162522a7f2cf9217a62
ferran <ferransaez2000@gmail.com>
2023-04-14 17:08:13 +0200

    #

---
+++ database/migrations/2023_04_14_144220_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->decimal('precio', 8, 2);
+            $table->unsignedBigInteger('id_categoria');
+            $table->timestamps();
+            $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
