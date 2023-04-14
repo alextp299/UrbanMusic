a291221eb73366668891b48e7703d5233cdc21cd
tyranxl <alexnador1408@gmail.com>
2023-04-14 17:39:02 +0200

    #

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -27,13 +27,13 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent"  v-else>
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <router-link to="/dashboard" class="nav-item nav-link text-dark mt-3 ms-5 fw-bol tamaño_letra">Categorias</router-link>
+                                <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5 fw-bold tamaño_letra">Categorias</router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link to="/dashboard" class="nav-item nav-link text-dark mt-3 ms-5 fw-bold tamaño_letra">Top Canciones</router-link>
+                                <router-link to="/dashboard" class="nav-item nav-link mt-3 ms-5 fw-bold tamaño_letra">Top Canciones</router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link to="/posts" class="nav-item nav-link text-dark mt-3 ms-5 fw-bold tamaño_letra">Merchandising Artistas</router-link>
+                                <router-link to="/posts" class="nav-item nav-link mt-3 ms-5 fw-bold tamaño_letra">Merchandising Artistas</router-link>
                             </li>
                         </ul>
                         <router-link to="/login" class="nav-item nav-link mt-3 me-5"><button class="btn btn-success fondo-color tamaño_session">Iniciar Sesión</button></router-link>
@@ -49,13 +49,13 @@
                 <nav class="navbar-light bg-light">
                 <div class="container-fluid row mb-4">
                     <div class="col-lg-4 col-md-4 mb-md-0">
-                        <router-link to="/dashboard" class="nav-item nav-link  conf_priv mt-3 fw-bold tamaño_letra">Política de Privacidad</router-link>
+                        <router-link to="/dashboard" class="nav-item nav-link conf_priv mt-3 fw-bold tamaño_letra">Política de Privacidad</router-link>
                     </div>
                     <div class="col-lg-4 col-md-4 mb-md-0">
-                        <router-link to="/dashboard" class="nav-item nav-link  conf_cookies mt-3 fw-bold tamaño_letra">Política de Cookies</router-link>
+                        <router-link to="/dashboard" class="nav-item nav-link conf_cookies mt-3 fw-bold tamaño_letra">Política de Cookies</router-link>
                     </div>
                     <div class="col-lg-4 col-md-4 mb-md-0">
-                        <router-link to="/dashboard" class="nav-item nav-link  conf_avis mt-3 fw-bold tamaño_letra">Aviso Legal</router-link>
+                        <router-link to="/dashboard" class="nav-item nav-link conf_avis mt-3 fw-bold tamaño_letra">Aviso Legal</router-link>
                     </div>
                 </div>
                 <!-- Section: Social media -->
