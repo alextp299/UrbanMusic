e132a7697946b93cb7a2ca276e77a6f97ec81ecc
tyranxl <alexnador1408@gmail.com>
2023-04-16 23:27:34 +0200

    #

--- resources/js/components/Aviso_Legal.vue
+++ resources/js/components/Aviso_Legal.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="container-fluid mt-5">
-    
+  <div class="container-fluid mt-5 mb-5">
+    <h3 class="mb-4">Aviso Legal</h3>
+    <p class="mt-2" style="font-size: 15px;">Este aviso legal establece los términos y condiciones para el uso de nuestro sitio web, Urban Music. Al acceder y utilizar nuestro sitio web, usted acepta estos términos y condiciones en su totalidad.</p>
+
+    <h5>1. Propiedad intelectual</h5>
+    <p class="mt-2" style="font-size: 15px;">Todos los derechos de propiedad intelectual relacionados con nuestro sitio web y su contenido son propiedad de Urban Music o de sus respectivos propietarios. Se prohíbe la reproducción, distribución, modificación, publicación o uso no autorizado de cualquier contenido de nuestro sitio web.</p>
+
+    <h5>2. Responsabilidad del usuario</h5>
+    <p class="mt-2" style="font-size: 15px;">El uso de nuestro sitio web es bajo su propia responsabilidad. Usted es responsable de garantizar que cualquier información que proporcione a nuestro sitio web sea precisa y no infrinja los derechos de terceros. También es responsable de cualquier uso no autorizado de su cuenta en nuestro sitio web.</p>
+
+    <h5>3. Responsabilidad de Urban Music</h5>
+    <p class="mt-2" style="font-size: 15px;">Nos esforzamos por garantizar que nuestro sitio web sea seguro y libre de errores, pero no podemos garantizar que nuestro sitio web esté libre de errores o virus informáticos. No somos responsables de ningún daño o pérdida que pueda surgir del uso de nuestro sitio web.</p>
+
+    <h5>4. Enlaces externos</h5>
+    <p class="mt-2" style="font-size: 15px;">Nuestro sitio web puede incluir enlaces a sitios web externos. No somos responsables del contenido o la seguridad de estos sitios web externos.</p>
+
+    <h5>5. Cambios en el sitio web</h5>
+    <p class="mt-2" style="font-size: 15px;">Nos reservamos el derecho de modificar o interrumpir nuestro sitio web en cualquier momento sin previo aviso. No somos responsables de ningún daño o pérdida que pueda surgir del cambio o interrupción de nuestro sitio web.</p>
+
+    <h5>6. Ley aplicable y jurisdicción</h5>
+    <p class="mt-2" style="font-size: 15px;">
+      Este aviso legal se rige por las leyes en cuestión y cualquier disputa que surja en relación con este aviso legal se resolverá exclusivamente en los tribunales.
+
+      Si tiene alguna pregunta o inquietud sobre nuestro aviso legal, por favor contáctenos.
+    </p>
   </div>
 </template>
 
