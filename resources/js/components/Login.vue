1daa4c58d6beeff2d6aca2d5846e1c4c9c33b2ea
tyranxl <alexnador1408@gmail.com>
2023-04-17 18:55:06 +0200

    #

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="container-fluid mt-5 mb-5">
         <div class="row justify-content-center align-items-center">
-            <div class="col-md-8">
+            <div class="col-md-4">
                 <div v-if="error !== null" class="alert alert-danger alert-dismissible fade show" role="alert">
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 
                     <strong>{{error}}</strong>
                 </div>
 
-                <div class="card card-default d-flex">
-                    <div class="card-header text-center"><h5>¡Inicia Sesión!</h5></div>
+                <div class="card card-default d-flex px-5 py-5">
+                    <div class="card-body p-1 text-center"><h5>¡Inicia Sesión!</h5></div>
                     <div class="card-body">
                         <form>
                             <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">Correo Electrónico</label>
+                                <label for="email" class="col-sm-12 col-form-label text-md-right">Correo Electrónico</label>
                                     <input id="email" type="email" class="form-control" v-model="email" required
                                            autofocus autocomplete="off" placeholder="Ingrese su Correo">
                             </div>
 
 
                             <div class="form-group row mt-1">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Constraseña</label>
+                                <label for="password" class="col-md-12 col-form-label text-md-right">Constraseña</label>
                                     <input id="password" type="password" class="form-control" v-model="password"
                                            required autocomplete="off" placeholder="Ingrese su Contraseña">
                             </div>
