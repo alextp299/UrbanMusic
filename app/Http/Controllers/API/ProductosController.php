d911a2a0614ea38974ae3085efc81729bf02828a
ferran <ferransaez2000@gmail.com>
2023-04-19 19:53:39 +0200

    #

--- app/Http/Controllers/API/ProductosController.php
+++ app/Http/Controllers/API/ProductosController.php
@@ -13,4 +13,14 @@ class ProductosController extends Controller
         $productos = Producto::where('id_categoria', 2)->get();
         return response()->json($productos);
     }
+    public function guardarProductosSeleccionados(Request $request)
+    {
+        // Obtener el array de productos seleccionados del request
+        $productosSeleccionados = $request->input('productos_seleccionados');
+
+        // Guardar el array en la sesión
+        session(['productos_seleccionados' => $productosSeleccionados]);
+        
+        return $request;
+    }
 }
