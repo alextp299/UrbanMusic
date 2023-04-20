8bf875035f2ff215bdd24cba666d8009160d7e0d
ferran <ferransaez2000@gmail.com>
2023-04-20 18:40:13 +0200

    #

--- app/Http/Controllers/API/QuevedoMusicController.php
+++ app/Http/Controllers/API/QuevedoMusicController.php
@@ -10,7 +10,37 @@ class QuevedoMusicController extends Controller
 {
     public function index()
     {
-        $canciones = Canciones::all();
+        $canciones = Canciones::where('id_categoria_cancion', 1)->get();
+        return response()->json($canciones);
+    }
+
+    public function badbunny()
+    {
+        $canciones = Canciones::where('id_categoria_cancion', 2)->get();
+        return response()->json($canciones);
+    }
+
+    public function shakira()
+    {
+        $canciones = Canciones::where('id_categoria_cancion', 3)->get();
+        return response()->json($canciones);
+    }
+
+    public function rosalia()
+    {
+        $canciones = Canciones::where('id_categoria_cancion', 4)->get();
+        return response()->json($canciones);
+    }
+
+    public function eladio()
+    {
+        $canciones = Canciones::where('id_categoria_cancion', 5)->get();
+        return response()->json($canciones);
+    }
+
+    public function karol()
+    {
+        $canciones = Canciones::where('id_categoria_cancion', 6)->get();
         return response()->json($canciones);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,17 +22,16 @@ Route::post('login',[UserController::class,'login']);
 Route::post('register',[UserController::class,'register']);
 Route::post('logout',[UserController::class,'logout'])->middleware(['auth:sanctum']);
 
-Route::group(['prefix' => 'canciones'], function (){
-    Route::get('/', [QuevedoMusicController::class, 'index']);
-});
 
+Route::get('Quevedo', [QuevedoMusicController::class, 'index']);
+
+Route::get('Bad Bunny', [QuevedoMusicController::class, 'badbunny']);
+    
 Route::group(['prefix' => 'productos'], function (){
     Route::get('/', [ProductosController::class, 'index']);
 });
 
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
+Route::get('carrito', [ProductosController::class, 'guardarProductosSeleccionados']);
 
 Route::group(['prefix' => 'posts', 'middleware' => 'auth:sanctum'], function (){
     Route::get('/', [PostController::class, 'index']);
