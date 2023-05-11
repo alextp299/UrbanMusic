21857c3ede053838b384d2766a326a3ded377b12
tyranxl <alexnador1408@gmail.com>
2023-05-11 20:06:45 +0200

    #

--- resources/js/components/Merchandising.vue
+++ resources/js/components/Merchandising.vue
@@ -4,34 +4,28 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card espaciado-foto mb-5 container">
-                    <img src="../../img/Artistas/badbunny.webp" class="card-img-top3 image" alt="Merchandising Bad Bunny">
-                    <div class="middle3">
-                        <router-link to="/merchandising/BadBunny" class="nav-item nav-link mt-3"><button class="fondo-color tamaño_session">Entrar</button></router-link>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Bad Bunny</h5>
+                    <router-link to="/merchandising/BadBunny"><img src="../../img/Artistas/badbunny.webp" class="card-img-top3 image" alt="Merchandising Bad Bunny"></router-link>
+                    <div class="card-body d-flex justify-content-between">
+                        <h5 class="card-title p-2">Bad Bunny</h5>
+                        <router-link to="/merchandising/BadBunny" class="nav-item nav-link"><button class="fondo-color tamaño_session">Entrar</button></router-link>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card espaciado-foto mb-5 container">
-                    <img src="../../img/Artistas/rosalia.webp" class="card-img-top3 image" alt="Merchandising Rosalia">
-                    <div class="middle3">
-                        <router-link to="/merchandising/Rosalia" class="nav-item nav-link mt-3"><button class="fondo-color tamaño_session">Entrar</button></router-link>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Rosalia</h5>
+                    <router-link to="/merchandising/Rosalia"><img src="../../img/Artistas/rosalia.webp" class="card-img-top3 image" alt="Merchandising Rosalia"></router-link>
+                    <div class="card-body d-flex justify-content-between">
+                        <h5 class="card-title p-2">Rosalia</h5>
+                        <router-link to="/merchandising/Rosalia" class="nav-item nav-link"><button class="fondo-color tamaño_session">Entrar</button></router-link>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card espaciado-foto mb-5 container">
-                    <img src="../../img/Artistas/eladio.webp" class="card-img-top3 image" alt="Merchandising Eladio Carrión">
-                    <div class="middle3">
-                        <router-link to="/merchandising/EladioCarrion" class="nav-item nav-link mt-3"><button class="fondo-color tamaño_session">Entrar</button></router-link>
-                    </div>
-                    <div class="card-body">
+                    <router-link to="/merchandising/EladioCarrion"><img src="../../img/Artistas/eladio.webp" class="card-img-top3 image" alt="Merchandising Eladio Carrión"></router-link>
+                    <div class="card-body d-flex justify-content-between">
                         <h5 class="card-title">Eladio Carrión</h5>
+                        <router-link to="/merchandising/EladioCarrion" class="nav-item nav-link"><button class="fondo-color tamaño_session">Entrar</button></router-link>
                     </div>
                 </div>
             </div>
