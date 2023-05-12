2a6b879a58e150443837b39607c2c2f5916968b7
Ferran <ferransaez2000@gmail.com>
2023-05-12 22:34:41 +0200

    #

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -39,6 +39,27 @@
 </div>
 <div class="container-fluid mt-5">
   <h3 class="mb-4">Artistas más escuchados</h3>
+  <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   <div class="row">
     <div class="col-md-4">
       <div class="card espaciado-foto mb-5 container">
@@ -120,11 +141,26 @@
 </template>
 
 <script>
-export default{
-    name: "Home"
-}
+export default {
+  name: "Home",
+  mounted() {
+    if (!localStorage.getItem("modalShown")) {
+      setTimeout(() => {
+        this.showModal();
+        localStorage.setItem("modalShown", true);
+      }, 10000);
+    }
+  },
+  methods: {
+    showModal() {
+      const modal = document.getElementById("exampleModal");
+      const bootstrapModal = new bootstrap.Modal(modal);
+      bootstrapModal.show();
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+/* Estilos adicionales si es necesario */
 </style>
