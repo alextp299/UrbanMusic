76a9b2004d7521d563b71e32277e81e6b1b6c73c
tyranxl <alexnador1408@gmail.com>
2023-06-01 10:36:13 +0200

    #

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,6 @@ Route::post('login',[UserController::class,'login']);
 Route::post('register',[UserController::class,'register']);
 Route::post('logout',[UserController::class,'logout'])->middleware(['auth:sanctum']);
 
-//All
-Route::get('usuariosAdmin', [UserController::class,'usuarios']);
-Route::get('canciones', [MusicController::class, 'canciones']);
-Route::get('productos',[ProductosController::class,'productos']);
-
 //Categoria + Canciones
 Route::get('canciones_categoria', [MusicController::class, 'canciones_categoria']);
 Route::get('/canciones_categoria/{id_categoria}', [MusicController::class, 'getCancionesCategoria']);
@@ -36,32 +31,55 @@ Route::get('productos_categoria', [ProductosController::class, 'productos_catego
 Route::get('/productos_categoria/{id_categoria}', [ProductosController::class, 'getProductosCategoria']);
 
 //Carrito
-Route::post('pedidos',[ProductosController::class,'pedidos']);
-Route::get('carrito', [ProductosController::class, 'obtenerProductos']);
-Route::get('precioTotal', [ProductosController::class, 'obtenerPrecioTotal']);
-Route::post('guardar-productos-en-session', [\App\Http\Controllers\API\ProductosController::class, 'guardarProductosSeleccionados']);
-Route::delete('eliminar/{id}', [ProductosController::class,'eliminarProductos']);
+Route::group(['prefix' => '/', 'middleware' => 'auth:sanctum'], function () {
+
+    Route::post('pedidos',[ProductosController::class,'pedidos']);
+    Route::get('carrito', [ProductosController::class, 'obtenerProductos']);
+    Route::get('precioTotal', [ProductosController::class, 'obtenerPrecioTotal']);
+    Route::post('guardar-productos-en-session', [\App\Http\Controllers\API\ProductosController::class, 'guardarProductosSeleccionados']);
+    Route::delete('eliminar/{id}', [ProductosController::class,'eliminarProductos']);
+
+});
 
 //Admin Producto
-Route::post('addProducto', [ProductosController::class, 'agregarProductos']);
-Route::delete('deleteProducto/{id}', [ProductosController::class,'delete']);
-Route::post('updateProducto/{id}', [ProductosController::class,'update']);
-Route::get('editProducto/{id}', [ProductosController::class,'edit']);
+Route::group(['prefix' => '/', 'middleware' => 'auth:sanctum'], function () {
+
+    Route::get('productos',[ProductosController::class,'productos']);
+    Route::post('addProducto', [ProductosController::class, 'agregarProductos']);
+    Route::delete('deleteProducto/{id}', [ProductosController::class,'delete']);
+    Route::post('updateProducto/{id}', [ProductosController::class,'update']);
+    Route::get('editProducto/{id}', [ProductosController::class,'edit']);
+
+});
 
 //Admin Canciones
-Route::post('addCanciones', [MusicController::class, 'agregarCanciones']);
-Route::delete('delete/{id}', [MusicController::class,'delete']);
-Route::post('update/{id}', [MusicController::class,'update']);
-Route::get('edit/{id}', [MusicController::class,'edit']);
+Route::group(['prefix' => '/', 'middleware' => 'auth:sanctum'], function () {
+
+    Route::get('canciones', [MusicController::class, 'canciones']);
+    Route::post('addCanciones', [MusicController::class, 'agregarCanciones']);
+    Route::delete('delete/{id}', [MusicController::class,'delete']);
+    Route::post('update/{id}', [MusicController::class,'update']);
+    Route::get('edit/{id}', [MusicController::class,'edit']);
+
+});
 
 //Admin Users
-Route::delete('deleteUsuarioAdmin/{id}', [UserController::class,'deleteUsuarioAdmin']);
-Route::post('addUsuarioAdmin', [UserController::class, 'agregarUsuariosAdmin']);
-Route::post('updateUserAdmin/{id}', [UserController::class, 'editarUsuariosAdmin']);
-Route::get('editUser/{id}', [UserController::class, 'editUserAdmin']);
-Route::get('roles', [UserController::class, 'getRoles']);
+Route::group(['prefix' => '/', 'middleware' => 'auth:sanctum'], function () {
+
+    Route::get('usuariosAdmin', [UserController::class,'usuarios']);
+    Route::delete('deleteUsuarioAdmin/{id}', [UserController::class,'deleteUsuarioAdmin']);
+    Route::post('addUsuarioAdmin', [UserController::class, 'agregarUsuariosAdmin']);
+    Route::post('updateUserAdmin/{id}', [UserController::class, 'editarUsuariosAdmin']);
+    Route::get('editUser/{id}', [UserController::class, 'editUserAdmin']);
+    Route::get('roles', [UserController::class, 'getRoles']);
+
+});
 
 
-//User 
-Route::post('updateUsuario/{id}', [UserController::class,'update']);
-Route::delete('deleteUsuario/{id}', [UserController::class,'delete']);
+//User
+Route::group(['prefix' => '/', 'middleware' => 'auth:sanctum'], function () {
+
+    Route::post('updateUsuario/{id}', [UserController::class,'update']);
+    Route::delete('deleteUsuario/{id}', [UserController::class,'delete']);
+
+});
